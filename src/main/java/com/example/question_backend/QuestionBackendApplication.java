fabab16bcfa17ab56db39212b0c8b56072bd972c
yiting <yiting50425@gmail.com>
2023-06-20 15:26:11 +0800

    Spring init

---
+++ src/main/java/com/example/question_backend/QuestionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.question_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/question_backend/QuestionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.question_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
